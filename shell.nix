53423339739895c82a77b99d9269d144bf71a50b
John Axel Eriksson <john@insane.se>
2020-05-25 20:44:02 +0200

    Use niv for nix dependency mgmt

--- shell.nix
+++ shell.nix
@@ -1,23 +1,25 @@
 let
-  mozilla = import (builtins.fetchGit {
-      url = "https://github.com/mozilla/nixpkgs-mozilla.git";
-      ref = "master";
-      rev = "b52a8b7de89b1fac49302cbaffd4caed4551515f";
-  });
+  pkgs = import ./nix { };
+  rustChannel = pkgs.latest.rustChannels.stable;
 
-  pkgs = (import <nixpkgs> { }).extend(mozilla);
-  rustChannel = pkgs.rustChannelOf { date = "2019-10-21";
-                                     channel = "nightly";
-                                   };
+  rust = rustChannel.rust.override {
+    extensions = [ "rust-src" "clippy-preview" "rustfmt-preview" ];
+  };
+
+  cargo = rustChannel.cargo;
+
 in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    niv
+    rust
+    cargo
 
-  with pkgs; mkShell {
-   buildInputs = [
-               (rustChannel.rust.override { extensions = [ "clippy-preview" ]; })
-               gcc
-               openssl
-               pkg-config
-               skim
-   ] ++ (if pkgs.rust-analyzer != null then [ rust-analyzer ] else []);
-   RUST_SRC_PATH = "${rustChannel.rust-src}/lib/rustlib/src/rust/src";
-  }
+    gcc
+    openssl
+    pkg-config
+
+    skim
+  ];
+  RUST_SRC_PATH = "${rustChannel.rust-src}/lib/rustlib/src/rust/src";
+}
